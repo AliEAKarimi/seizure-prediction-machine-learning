35f510c469bb6b167ce0d876774414fe0887e360
Ali Karimi <alieakarimi@gmail.com>
2022-12-21 15:02:53 +0330

    using KFold  instead of the train test split

--- experiments.py
+++ experiments.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import accuracy_score, recall_score, precision_score
 import matplotlib.pyplot as plt
 import pickle
@@ -20,8 +20,8 @@ y = pickle.load(open('y.pkl', 'rb'))
 
 x_normal = np.concatenate((x[:300], x[400:]), axis=0)
 x_seizure = x[300:400]
-print(x_normal.shape)
-print(x_seizure.shape)
+# print(x_normal.shape)
+# print(x_seizure.shape)
 sampling_freq = 173.6 #based on info from website
 
 b, a = butter(3, [0.5,40], btype='bandpass',fs=sampling_freq)
@@ -29,8 +29,8 @@ b, a = butter(3, [0.5,40], btype='bandpass',fs=sampling_freq)
 
 x_normal_filtered = np.array([lfilter(b,a,x_normal[ind,:]) for ind in range(x_normal.shape[0])])
 x_seizure_filtered = np.array([lfilter(b,a,x_seizure[ind,:]) for ind in range(x_seizure.shape[0])])
-print(x_normal.shape)
-print(x_seizure.shape)
+# print(x_normal.shape)
+# print(x_seizure.shape)
 
 
 x_normal = x_normal_filtered
@@ -39,12 +39,19 @@ x_seizure = x_seizure_filtered
 x = np.concatenate((x_normal,x_seizure))
 y = np.concatenate((np.zeros((400,1)),np.ones((100,1))))
 
-print(x.shape)
-print(y.shape)
+# print(x.shape)
+# print(y.shape)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=seed,test_size=0.2)
+# x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=seed,test_size=0.2)
 
-print(x_test.shape)
+kf  = KFold(n_splits=5,random_state=seed,shuffle=True)
+kf.get_n_splits(x)
+# print(kf)
+for train_index, test_index in kf.split(x):
+    # print("TRAIN:", train_index, "TEST:", test_index)
+    x_train, x_test = x[train_index], x[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+# print(x_test.shape)
 
 clf = SVC(kernel='linear')
 clf.fit(x_train, y_train)
