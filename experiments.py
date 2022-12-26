d54a2febc2194ce56b277ccd3e120d02710eaf62
Ali Karimi <alieakarimi@gmail.com>
2022-12-26 05:26:11 +0330

    rbf svm for raw data with using of KFold splitter

--- experiments.py
+++ experiments.py
@@ -234,6 +234,8 @@ evaluation(yy_test,svm_y_pred)
 rbf_svm_clf.fit(x_train, y_train)
 svm_y_pred = rbf_svm_clf.predict(x_test)
 evaluation(y_test,svm_y_pred)
+# svm with rbf kernel that used of cross validation(k_fold) for splitting data
+cross_validation(rbf_svm_clf, x, y)
 
 # svm with poly kernel
 poly_svm_clf = SVC(kernel='poly')
