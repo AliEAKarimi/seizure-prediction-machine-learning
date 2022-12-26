2739b2a7f336821ed015bc84e6db3c4e5152cf63
Ali Karimi <alieakarimi@gmail.com>
2022-12-26 05:25:07 +0330

    rbf svm for visualized data with using of train_test_split

--- experiments.py
+++ experiments.py
@@ -230,6 +230,10 @@ rbf_svm_clf = SVC(kernel='rbf')
 rbf_svm_clf.fit(xx_train, yy_train)
 svm_y_pred = rbf_svm_clf.predict(xx_test)
 evaluation(yy_test,svm_y_pred)
+# svm with rbf kernel that used of train_test_split for splitting visualized data
+rbf_svm_clf.fit(x_train, y_train)
+svm_y_pred = rbf_svm_clf.predict(x_test)
+evaluation(y_test,svm_y_pred)
 
 # svm with poly kernel
 poly_svm_clf = SVC(kernel='poly')
